1065b26b37b9576923972d3a6b273aab28821cf7
Soubhagya Sampanna Mohanty <58468005+soubhagyamohanty@users.noreply.github.com>
2026-06-07 11:23:25 +0530

    Update Test.java

--- src/com/soubhagya/java/basics/Test.java
+++ src/com/soubhagya/java/basics/Test.java
@@ -7,6 +7,7 @@ public class Test {
         System.out.println(Object.class.getDeclaredMethods().length);
         for (Method method: Object.class.getDeclaredMethods()) {
             System.out.println(method.getName());
+            System.out.println("commit changes on github online");
 
         }
     }
